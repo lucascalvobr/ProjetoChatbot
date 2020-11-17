5a3737fc99038aec1a4110d505936e7384b1acbe
94andradeflavio <94andrade.flavio@gmail.com>
2020-11-17 20:35:44 -0300

    adicionado arquivos PHP do chatBot

---
+++ bot/chat.php
@@ -0,0 +1,44 @@
+<?php
+include "Bot.php";
+include "Cotacao.php";
+
+$bot = new Bot;
+$questions = [
+    "php" => "É uma linguagem de programação Server Side",
+    "linux" => "É um sistema operacional desenvolvido por Linus Torvald",
+    "dns" => "O DNS (Domain Name System) é um sistema de gestão de nomes para computadores",
+    "chatbot" => "É um programa de computador que tenta simular um ser humano na conversação com as pessoas",
+    "qual seu nome" => "Meu nome é " . $bot->getName(),
+];
+
+/*
+Carregamento de dados via JSON
+
+$obj = json_decode(file_get_contents('regras.json'), True);
+$questions = array();
+foreach ($obj as $values) {
+    foreach ($values as $key => $value) {
+        $questions[$key] = $value;
+    }
+}
+*/
+
+if (isset($_GET['msg'])) {
+    $msg = strtolower($_GET['msg']);
+    $bot->hears($msg, function (Bot $botty) {
+        global $msg;
+        global $questions;
+
+        
+
+
+        $generics = ['oi', 'oie', 'ola', 'olá', 'bom dia', 'boa tarde', 'boa noite'];
+        if (in_array($msg, $generics)) {
+            $botty->reply('Olá. Em que posso ajudar?');
+        } elseif ($botty->ask($msg, $questions) == "") {
+            $botty->reply("Desculpe, não entendi.");
+        } else {
+            $botty->reply($botty->ask($msg, $questions));
+        }
+    });
+}
